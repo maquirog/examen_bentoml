714992a5528ac0c6f414e047329e49311d8fb7a1
Ubuntu <ubuntu@ip-172-31-17-223.eu-west-1.compute.internal>
2025-03-29 11:05:59 +0000

    setup.sh change

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 virtualenv venv
+echo "Waiting 10 seconds"
+sleep 10
 source venv/bin/activate
 pip3 install -r requirements.txt
 cd ../
 docker load --input bento_image.tar
 docker images
 docker run --rm -d -p 3000:3000 quiroga_examen_bentoml_service:gqln4xqmowjq766n
+echo "Waiting 10 seconds"
+sleep 10
 cd examen_bentoml/src/
-python -m pytest -o log_cli=1 -o log_cli_level=INFO test.py
+python3 -m pytest -o log_cli=1 -o log_cli_level=INFO test.py
