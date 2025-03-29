3ad2c251f35695b9c9a3d40c1d98959dbd9cb5e0
Ubuntu <ubuntu@ip-172-31-23-209.eu-west-1.compute.internal>
2025-03-29 10:56:45 +0000

    setup.sh change

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 virtualenv venv
 source venv/bin/activate
-cd examen_bentoml/
 pip3 install -r requirements.txt
 cd ../
 docker load --input bento_image.tar
+docker images
 docker run --rm -d -p 3000:3000 quiroga_examen_bentoml_service:gqln4xqmowjq766n
+cd examen_bentoml/src/
 python -m pytest -o log_cli=1 -o log_cli_level=INFO test.py
